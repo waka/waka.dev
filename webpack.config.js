eb710b35f05369ab16b90c6d1b4d2dcedf20cc13
yo_waka <y.wakahara@gmail.com>
2023-07-29 23:56:44 +0900

    update libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  devtool: "cheap-module-source-map",
   target: "webworker",
   mode: process.env.NODE_ENV || "production",
   context: __dirname,
@@ -10,20 +9,21 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        use: [
-          { loader: "ts-loader" }
-        ],
-        exclude: /node_modules/
-      }
-    ]
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       "process.env.BUILD_DATE": JSON.stringify(process.env.BUILD_DATE),
-      "process.env.GH_ACCESS_TOKEN": JSON.stringify(process.env.GH_ACCESS_TOKEN)
-    })
+      "process.env.GH_ACCESS_TOKEN": JSON.stringify(
+        process.env.GH_ACCESS_TOKEN
+      ),
+    }),
   ],
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devtool: "cheap-module-source-map",
 };
